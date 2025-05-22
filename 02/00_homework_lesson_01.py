20699b5d2d75b7d8cbdf4c53cbca9babdbd0b90e
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2025-05-22 17:57:27 +0200

    adding file

---
+++ 02/00_homework_lesson_01.py
@@ -0,0 +1,9 @@
+print("Mini Calculator")
+
+a = float(input("First number: "))
+b = float(input("Second number: "))
+
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Quotient:", a / b)  # division by zero
