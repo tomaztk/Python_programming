fddc4a5648f1f07645199acb7b36c95af8ca79b5
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2025-06-09 21:07:36 +0200

    adding trace

---
+++ 07/sample.py
@@ -0,0 +1,9 @@
+# Defining a decorator
+def trace(f):
+    def wrap(*args, **kwargs):
+        print(f"[TRACE] func: {f.__name__}, args: {args}, kwargs: {kwargs}")
+        return f(*args, **kwargs)
+
+    return wrap
+
+print(trace.__name__)  
