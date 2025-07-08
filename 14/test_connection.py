49f5eff56bf8859ccd5bc54b9c7fc6d9daa21f6e
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2025-07-08 21:32:39 +0200

    update

--- 14/test_connection.py
+++ 14/test_connection.py
@@ -1,15 +1,42 @@
 import sqlite3
 
-# con = sqlite3.connect("/Users/tomazkastrun/Documents/tomaztk_github/Python_programming/14/sample.db")
-con = sqlite3.connect("sample.db")
+# one way :)
+'''
+con = sqlite3.connect("shop.db")
 cur = con.cursor()
-#res = cur.execute("SELECT * FROM Customers")
-#res.fetchone()
-#CustomerID, Name, Email = res.fetchone()
-#print(f'The Customer is {CustomerID!r}, with name {Name} and email {Email}')
-#con.close()
+
+query = 
+SELECT
+    Customers.Name AS Customer,
+    Products.Name AS Product,
+    Orders.Quantity,
+    Orders.OrderDate
+FROM Orders
+JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+JOIN Products ON Orders.ProductID = Products.ProductID;
 
 
-for row in cur.execute("SELECT * FROM Customers"):
+for row in cur.execute(query):
     print(row)
 con.close()
+'''
+
+
+# or another way :)
+
+# con = sqlite3.connect("/Users/tomazkastrun/Documents/tomaztk_github/Python_programming/14/sample.db")
+con = sqlite3.connect("shop.db")
+cur = con.cursor()
+res = cur.execute("SELECT * FROM Customers")
+df = res.fetchall()
+#CustomerID, Name, Email = res.fetchone()
+#print(f'The Customer is {CustomerID!r}, with name {Name} and email {Email}')
+con.close()
+
+
+# print(df)
+import pandas as pd 
+
+
+pd_df = pd.DataFrame(df, columns=['CustomerID', 'Name', 'Email'])
+print(pd_df)
